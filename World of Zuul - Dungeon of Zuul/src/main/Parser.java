43b1165f8112d23c7f83691ce148502cff3292fb
Devulak <do0l67@gmail.com>
2016-09-28 15:35:09 +0200

    Wat

--- World of Zuul - Dungeon of Zuul/src/main/Parser.java
+++ World of Zuul - Dungeon of Zuul/src/main/Parser.java
@@ -5,38 +5,40 @@ import java.util.StringTokenizer;
 
 public class Parser 
 {
-    private CommandWords commands;
-    private Scanner reader;
+	private CommandWords commands;
+	private Scanner reader;
 
-    public Parser() 
-    {
-        commands = new CommandWords();
-        reader = new Scanner(System.in);
-    }
+	public Parser() 
+	{
+		commands = new CommandWords();
+		reader = new Scanner(System.in);
+	}
 
-    public Command getCommand() 
-    {
-        String inputLine;
-        String word1 = null;
-        String word2 = null;
+	public Command getCommand() 
+	{
+		String inputLine;
+		String word1 = null;
+		String word2 = null;
 
-        System.out.print("> "); 
+		System.out.print("> "); 
 
-        inputLine = reader.nextLine();
+		inputLine = reader.nextLine();
 
-        Scanner tokenizer = new Scanner(inputLine);
-        if(tokenizer.hasNext()) {
-            word1 = tokenizer.next();
-            if(tokenizer.hasNext()) {
-                word2 = tokenizer.next(); 
-            }
-        }
+		Scanner tokenizer = new Scanner(inputLine);
+		if(tokenizer.hasNext())
+		{
+			word1 = tokenizer.next();
+			if(tokenizer.hasNext())
+			{
+				word2 = tokenizer.next(); 
+			}
+		}
 
-        return new Command(commands.getCommandWord(word1), word2);
-    }
+		return new Command(commands.getCommandWord(word1), word2);
+	}
 
-    public void showCommands()
-    {
-        commands.showAll();
-    }
+	public void showCommands()
+	{
+		commands.showAll();
+	}
 }
