bbbbb8a392a3c4b32e22cb7aafbaa36273b94a1f
Devulak <do0l67@gmail.com>
2016-09-28 15:17:16 +0200

    Added files

---
+++ World of Zuul - Dungeon of Zuul/src/main/CommandWord.java
@@ -0,0 +1,18 @@
+package main;
+
+public enum CommandWord
+{
+    GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?");
+    
+    private String commandString;
+    
+    CommandWord(String commandString)
+    {
+        this.commandString = commandString;
+    }
+    
+    public String toString()
+    {
+        return commandString;
+    }
+}
--- World of Zuul - Dungeon of Zuul/src/main/Main.java
+++ World of Zuul - Dungeon of Zuul/src/main/Main.java
@@ -9,14 +9,16 @@ package main;
  *
  * @author ZeroXitreo
  */
-public class Main {
-
+public class Main
+{
 	/**
 	 * @param args the command line arguments
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO code application logic here
-                // Hej hej
+		Game game = new Game();
+		game.play();
 	}
 	
 }
