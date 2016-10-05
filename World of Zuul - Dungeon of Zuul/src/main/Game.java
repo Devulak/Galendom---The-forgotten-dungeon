b327d4d75b6c1b63b6ba9bb774db8090dfefddc2
NicolaiT <nicolai-1@live.dk>
2016-10-05 13:26:23 +0200

    Basic map
    Added 12 rooms called lvl_1, lvl_2, lvl_3 to lvl_8.
    Alternative rooms are called lvl_2b, lvl_3b to lvl_5b

--- World of Zuul - Dungeon of Zuul/src/main/Game.java
+++ World of Zuul - Dungeon of Zuul/src/main/Game.java
@@ -1,63 +1,83 @@
 package main;
 
-public class Game 
-{
+public class Game {
+
     private Parser parser;
     private Room currentRoom;
-        
 
-    public Game() 
-    {
+    public Game() {
         createRooms();
         parser = new Parser();
     }
 
+    private void createRooms() {
+        /* Creating rooms */
+        Room lvl_1, lvl_2, lvl_2b, lvl_3, lvl_3b, lvl_4, lvl_4b, lvl_5, lvl_5b, lvl_6, lvl_7, lvl_8;
 
-    private void createRooms()
-    {
-        Room outside, theatre, pub, lab, office, cave;
-      
-        outside = new Room("outside the main entrance of the university");
-        theatre = new Room("in a lecture theatre");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
-        cave = new Room("in a cave");
+        /* Give rooms descriptions. If you write "in a cave" then the game will say something like "You are in a cave" */
+        lvl_1 = new Room("lvl_1");
+        lvl_2 = new Room("lvl_2");
+        lvl_2b = new Room("lvl_2b");
+        lvl_3 = new Room("lvl_3");
+        lvl_3b = new Room("lvl_3b");
+        lvl_4 = new Room("lvl_4");
+        lvl_4b = new Room("lvl_4b");
+        lvl_5 = new Room("lvl_5");
+        lvl_5b = new Room("lvl_5b");
+        lvl_6 = new Room("lvl_6");
+        lvl_7 = new Room("lvl_7");
+        lvl_8 = new Room("lvl_8");
+
+        /* This gives the player the option to move between the rooms */
+        lvl_1.setExit("east", lvl_2);
+        lvl_1.setExit("west", lvl_2b);
+
+        lvl_2.setExit("south", lvl_3);
+        lvl_2.setExit("west", lvl_1);
+
+        lvl_2b.setExit("south", lvl_3b);
+        lvl_2b.setExit("east", lvl_1);
+
+        lvl_3.setExit("south", lvl_4);
+        lvl_3.setExit("north", lvl_2);
+
+        lvl_3b.setExit("east", lvl_4b);
+        lvl_3b.setExit("north", lvl_2b);
+
+        lvl_4.setExit("west", lvl_5);
+        lvl_4.setExit("south", lvl_5b);
+
+        lvl_4b.setExit("west", lvl_3b);
+
+        lvl_5.setExit("north", lvl_3);
+        lvl_5.setExit("west", lvl_6);
+        lvl_5.setExit("east", lvl_4);
+
+        lvl_5b.setExit("north", lvl_5);
+
+        lvl_6.setExit("south", lvl_7);
+        lvl_6.setExit("east", lvl_5);
         
-        outside.setExit("east", theatre);
-        outside.setExit("south", lab);
-        outside.setExit("west", pub);
-        outside.setExit("north", cave);
-
-        theatre.setExit("west", outside);
-
-        pub.setExit("east", outside);
-
-        lab.setExit("north", outside);
-        lab.setExit("east", office);
-
-        office.setExit("west", lab);
+        lvl_7.setExit("east", lvl_8);
+        lvl_7.setExit("north", lvl_6);
         
-        cave.setExit("south", outside);
-
-        currentRoom = outside;
+        lvl_8.setExit("west", lvl_7);
+        
+        currentRoom = lvl_1; //The player will start in this room
     }
 
-    public void play() 
-    {            
+    public void play() {
         printWelcome();
 
-                
         boolean finished = false;
-        while (! finished) {
+        while (!finished) {
             Command command = parser.getCommand();
             finished = processCommand(command);
         }
         System.out.println("Thank you for playing.  Good bye.");
     }
 
-    private void printWelcome()
-    {
+    private void printWelcome() {
         System.out.println();
         System.out.println("Welcome to the World of Zuul!");
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
@@ -66,31 +86,27 @@ public class Game
         System.out.println(currentRoom.getLongDescription());
     }
 
-    private boolean processCommand(Command command) 
-    {
+    private boolean processCommand(Command command) {
         boolean wantToQuit = false;
 
         CommandWord commandWord = command.getCommandWord();
 
-        if(commandWord == CommandWord.UNKNOWN) {
+        if (commandWord == CommandWord.UNKNOWN) {
             System.out.println("I don't know what you mean...");
             return false;
         }
 
         if (commandWord == CommandWord.HELP) {
             printHelp();
-        }
-        else if (commandWord == CommandWord.GO) {
+        } else if (commandWord == CommandWord.GO) {
             goRoom(command);
-        }
-        else if (commandWord == CommandWord.QUIT) {
+        } else if (commandWord == CommandWord.QUIT) {
             wantToQuit = quit(command);
         }
         return wantToQuit;
     }
 
-    private void printHelp() 
-    {
+    private void printHelp() {
         System.out.println("You are lost. You are alone. You wander");
         System.out.println("around at the university.");
         System.out.println();
@@ -98,9 +114,8 @@ public class Game
         parser.showCommands();
     }
 
-    private void goRoom(Command command) 
-    {
-        if(!command.hasSecondWord()) {
+    private void goRoom(Command command) {
+        if (!command.hasSecondWord()) {
             System.out.println("Go where?");
             return;
         }
@@ -111,20 +126,17 @@ public class Game
 
         if (nextRoom == null) {
             System.out.println("There is no door!");
-        }
-        else {
+        } else {
             currentRoom = nextRoom;
             System.out.println(currentRoom.getLongDescription());
         }
     }
 
-    private boolean quit(Command command) 
-    {
-        if(command.hasSecondWord()) {
+    private boolean quit(Command command) {
+        if (command.hasSecondWord()) {
             System.out.println("Quit what?");
             return false;
-        }
-        else {
+        } else {
             return true;
         }
     }
