cf3a77da5a0636805be5d16a9071d896233e8086
mohammadhassan0 <mohammad.tarek.hassan@outlook.com>
2016-10-06 10:48:25 +0200

    New characters v1

---
+++ World of Zuul - Dungeon of Zuul/src/main/Characters.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Characters {
+    
+}
+
+class monster {
+    String array [] = new String [Helm, Chest, Pants, Shoes, Weapons, Slot 1, Weapon, Slot 2, HealthPotions, Coins]
+    String inventory;
+    int health = 12;
+}
+
+class hero {
+    String array []= {"Helm, Chest, Pants, Shoes, Weapons Slot 1, Weapon Slot 2, Health Potions, Coins"};
+    int health = 12;
+    int level = 1;
+    int xp = 10; 
+    
+}
+
+class vendor {
+    String array []= {"Helm, Chest, Pants, Shoes, Weapons Slot 1, Weapon Slot 2, Health Potions, Coins"};
+    }
