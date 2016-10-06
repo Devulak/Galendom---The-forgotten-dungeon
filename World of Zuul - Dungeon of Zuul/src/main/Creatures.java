c55fb9a14b54a2cb6b28545f801721e894043382
mohammadhassan0 <mohammad.tarek.hassan@outlook.com>
2016-10-06 13:39:14 +0200

    Version 3 creatures

---
+++ World of Zuul - Dungeon of Zuul/src/main/Creatures.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Mohammad
+ */
+public class Creatures {
+    
+}
+
+class monster {
+    String[] inventory = new String [8]; //making inventory
+    int health = 12; //the health
+    int level = 1; //the level
+}
+    
+class hero {
+    String[] inventory = new String [8]; //making inventory
+    int health = 12; //the health
+    int level = 1; //the level
+    int xp = 10; //the xp
+    
+}
+
+class vendor {
+    String[] inventory = new String [8]; //mkaing inventory
+    }
