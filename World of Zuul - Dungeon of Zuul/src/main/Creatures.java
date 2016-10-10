11096c3f204e0184d8c8144facdc64c1fb1bbfab
mohammadhassan0 <mohammad.tarek.hassan@outlook.com>
2016-10-10 12:16:31 +0200

    version 5 creatures

--- World of Zuul - Dungeon of Zuul/src/main/Creatures.java
+++ World of Zuul - Dungeon of Zuul/src/main/Creatures.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
-/**
- *
- * @author Mohammad
- */
 public class Creatures {
-    
+
 }
 
 class monster {
@@ -20,9 +11,13 @@ class monster {
     /*Levelscaling, healthscaling
     *Can attack a certain amount of damage at the hero, which depends on the heros equipment.
     */
+    
+        
+    
 }
     
 class hero {
+    public static void heroinfo(){
     String[] inventory = new String [8]; //making inventory
     int health = 12; //the health
     int level = 1; //the level
@@ -31,6 +26,12 @@ class hero {
     //level up, xp
     //can hit with critical attacks
     //gear
+    switch (level){
+        case 0:
+            
+}
+}
+
     
 }
 
