9cb3e64a6006e901c7e03122233dad7002e8f133
Devulak <do0l67@gmail.com>
2016-11-24 17:36:26 +0100

    Item use and @Override mark

--- World of Zuul - Dungeon of Zuul/src/main/item/Item.java
+++ World of Zuul - Dungeon of Zuul/src/main/item/Item.java
@@ -4,20 +4,38 @@ public abstract class Item
 {
 	protected String name;
 	private int amount;
+	protected String extra;
+	protected int price;
 
-	public Item(String name, int amount) // Constructor
+	public Item(String name, int amount, int price) // Constructor
 	{
         this.name = name;
 		this.amount = amount;
+		this.price = price;
 	}
 
+	public Item(String name, int amount) // Constructor
+	{
+        this(name, amount, 0);
+	}
+
+	@Override
 	public String toString() // Return value on object
 	{
-		if(amount > 0)
-		{
-			return name + " (" + amount + ")";
-		}
-		return name;
+		String amount = this.amount > 0 ? " (" + this.amount + ")" : "";
+		String extra = this.extra != null ? " (" + this.extra + ")" : "";
+		String price = this.price > 0 ? " (" + this.price + ")" : "";
+		return name + amount + extra + price;
+	}
+
+	public int getPrice() // Get the amount of the item, if zero, means it's not stackable
+	{
+		return price;
+	}
+
+	public void clearPrice() // Get the amount of the item, if zero, means it's not stackable
+	{
+		price = 0;
 	}
 
 	public int getAmount() // Get the amount of the item, if zero, means it's not stackable
@@ -32,6 +50,11 @@ public abstract class Item
 
 	public void use()
 	{
-		amount--;
+		use(1);
+	}
+
+	public void use(int amount)
+	{
+		this.amount -= amount;
 	}
 }
