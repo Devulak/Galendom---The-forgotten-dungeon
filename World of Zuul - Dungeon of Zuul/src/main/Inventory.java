25acc739583804731674fcf3b7b781765318678e
Nicolai <nicolai-1@live.dk>
2016-11-28 22:22:05 +0100

    5

--- World of Zuul - Dungeon of Zuul/src/main/Inventory.java
+++ World of Zuul - Dungeon of Zuul/src/main/Inventory.java
@@ -8,7 +8,7 @@ public class Inventory
 	private List<Item> inventory = new ArrayList<>(); // Inventory full of stuff!
 	private boolean nolimit = false;
 	
-	public Inventory() // Constructor
+		public Inventory() // Constructor
 	{
 		
 	}
@@ -73,4 +73,63 @@ public class Inventory
 		return inventory;
 	}
 	
+	/**
+	 * Returns an Item object that is found in the inventory with the given
+	 * class to search, remember to put ".class" after naming what class you
+	 * would like to search for
+	 * 
+	 * @param searchClass
+	 * @return the Item found in the inventory or null if nothing found
+	 */
+	public Item searchItem(Class searchClass)
+	{
+		for (Item item : inventory)
+		{
+			if(searchClass.isInstance(item))
+			{
+				return item;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Uses an usable item in the inventory that's equal to it's parameter.
+	 * Returns true or false depending on if using the item was successful
+	 * 
+	 * @param searchForItem
+	 * @return true if the item provided can and has been used, false otherwise
+	 */
+	public boolean useItem(Item searchForItem)
+	{
+		return useItem(searchForItem, 1);
+	}
+	
+	/**
+	 * @param searchForItem
+	 * @param amount
+	 */
+	public boolean useItem(Item searchForItem, int amount)
+	{
+		if (searchForItem != null)
+		{
+			for (Iterator<Item> itInventory = inventory.iterator(); itInventory.hasNext();)
+			{
+				Item item = itInventory.next();
+				if (item.equals(searchForItem) && item.getAmount() >= amount) // Check to see if the item match and if it's usable, by how many
+				{
+					if (item.getAmount() > amount)
+					{
+						item.use(amount);
+					}
+					else
+					{
+						itInventory.remove();
+					}
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
