89f95c4e5da441ebc711c09ae52fe9c1582ce989
Nicolai <nicolai-1@live.dk>
2016-11-28 22:43:49 +0100

    12

---
+++ World of Zuul - Dungeon of Zuul/src/main/item/Chestplate.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package main.item;
+
+/**
+ *
+ * @author Nicolai
+ */
+public class Chestplate extends Armour
+{
+	public Chestplate(String name, int armour, int price) // Constructor
+	{
+		super(name, armour, price);
+	}
+	
+	public Chestplate(String name, int armour) // Constructor
+	{
+		this(name, armour, 0);
+	}
+}
