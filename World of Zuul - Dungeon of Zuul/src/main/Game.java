d9039353ff3d7388021e00f005d7da6b97f6f26d
Devulak <do0l67@gmail.com>
2016-12-12 11:17:14 +0100

    Moved creature to match structure

--- World of Zuul - Dungeon of Zuul/src/main/Game.java
+++ World of Zuul - Dungeon of Zuul/src/main/Game.java
@@ -5,10 +5,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import main.creature.*;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import main.creature.*;
 import main.item.*;
 import main.item.armour.*;
 
