19cc08adf3c1d436f98d8e7d8d9fdc8ede1b6966
mohammadhassan0 <mohammad.tarek.hassan@outlook.com>
2016-12-18 19:08:49 +0100

    Description change

--- Galendom - The forgotten dungeon/src/main/Game.java
+++ Galendom - The forgotten dungeon/src/main/Game.java
@@ -434,12 +434,9 @@ public class Game implements GameInterface
 	 * This method will print out when you start the game
 	 */
 	private void printWelcome() {
-		addDialogue("You awaken, hearing only silence. Without knowing where you are, you look to the right, and see a torch besides a skull and a Sign, which says:");
-		addDialogue("You're lost in the Dungeon of Zuul. You have to navigate through the rooms to find the exits.''");
-		addDialogue("You smell a disgusting stench, and look forward and see a big dark-green creature looking angry towards you.");
-		addDialogue("Your goal is now to move to the end of the map. At the end of the map, you will meet the last boss, the Fallen Knight.");
-		addDialogue("If you manage to defeat the boss, you will win, and escape this forgotten place.");
-		addDialogue("But if your health reaches zero, you will lose and your corpse will forever be forgotten. Now, go!");
+		addDialogue("Your goal is to move to the end of the map.");
+		addDialogue("If you manage to defeat the boss at the end, you will win.");
+		addDialogue("But if your health reaches zero, you will lose!");
 	}
 
 	/**
